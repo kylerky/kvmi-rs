6a3cde6d6a91ee42ccccbe654290de39c00775ee
Jinyi Xian <kylerky@live.com>
2020-03-26 14:44:10 +0000

    Trace syscalls to rename files

--- observer/src/collect/bp_handlers/file.rs
+++ observer/src/collect/bp_handlers/file.rs
@@ -1,12 +1,15 @@
 use kvmi_semantic::event::*;
+use kvmi_semantic::memory::address_space::{IA32eAddrT, IA32eVirtual};
 use kvmi_semantic::memory::{self, handle_table};
 use kvmi_semantic::tracing::functions::MSx64;
-use kvmi_semantic::{Domain, Error};
+use kvmi_semantic::{Domain, Error, RekallProfile};
 
 use crate::kvmi_capnp::event;
 use crate::kvmi_capnp::FileAccess;
 
 use futures::future::{BoxFuture, FutureExt};
+
+use std::convert::TryInto;
 
 use log::error;
 
@@ -17,6 +20,12 @@ use crate::collect::LogChT;
 const FILE_INFORMATION_CLASS: &str = "_FILE_INFORMATION_CLASS";
 const DISPOSITION_INFO: &str = "FileDispositionInformation";
 const DISPOSITION_INFO_EX: &str = "FileDispositionInformationEx";
+const RENAME_INFO: &str = "FileRenameInformation";
+
+struct SetInfoParams {
+    file_handle: u64,
+    file_info: IA32eAddrT,
+}
 
 pub(crate) fn read<'a>(
     dom: &'a mut Domain,
@@ -164,6 +173,7 @@ async fn set_info_(
         .rev_enums
         .get(FILE_INFORMATION_CLASS)
         .ok_or_else(|| Error::Profile(format!("Missing enum {}", FILE_INFORMATION_CLASS)))?;
+
     let dispos = *info_class.get(DISPOSITION_INFO).ok_or_else(|| {
         Error::Profile(format!(
             "Missing enum variant {}::{}",
@@ -174,6 +184,12 @@ async fn set_info_(
         Error::Profile(format!(
             "Missing enum variant {}::{}",
             FILE_INFORMATION_CLASS, DISPOSITION_INFO_EX,
+        ))
+    })? as u64;
+    let rename = *info_class.get(RENAME_INFO).ok_or_else(|| {
+        Error::Profile(format!(
+            "Missing enum variant {}::{}",
+            FILE_INFORMATION_CLASS, RENAME_INFO,
         ))
     })? as u64;
 
@@ -188,6 +204,13 @@ async fn set_info_(
 
     let res = if file_info_class == dispos || file_info_class == dispos_ex {
         handle_rm(dom, event, extra, log_tx, enable_ss, orig, file_handle).await
+    } else if file_info_class == rename {
+        let file_info = *args.get(2).unwrap();
+        let params = SetInfoParams {
+            file_info,
+            file_handle,
+        };
+        handle_mv(dom, event, extra, log_tx, enable_ss, orig, params).await
     } else {
         dom.resume_from_bp(orig, event, extra, enable_ss).await?;
         Ok(())
@@ -242,4 +265,109 @@ async fn handle_rm(
 
     log_tx.send(message).await;
     Ok(())
+}
+
+async fn handle_mv(
+    dom: &mut Domain,
+    event: &Event,
+    extra: &KvmiEventBreakpoint,
+    log_tx: &Sender<LogChT>,
+    enable_ss: bool,
+    orig: u8,
+    params: SetInfoParams,
+) -> Result<(), Error> {
+    let sregs = &event.get_arch().sregs;
+    let v_space = dom.get_vspace(kvmi_semantic::get_ptb_from(sregs)).clone();
+
+    let (process, pid, ppid, proc_name) = super::get_process(dom, event, sregs).await?;
+    let profile = dom.get_profile();
+    let file_handle = params.file_handle;
+    let file_obj_ptr = handle_table::get_obj_by(&v_space, file_handle, process, profile).await?;
+
+    let body_rva = profile.get_struct_field_offset("_OBJECT_HEADER", "Body")?;
+    let name_rva = profile.get_struct_field_offset("_FILE_OBJECT", "FileName")?;
+    let fname = memory::read_utf16(&v_space, file_obj_ptr + body_rva + name_rva).await?;
+
+    let file_info = params.file_info;
+    let name_len = v_space.read(file_info + 16, 4).await?;
+    let name_len = u32::from_ne_bytes(name_len[..].try_into().unwrap()) as usize;
+
+    let buffer = v_space.read(file_info + 16 + 4, name_len).await?;
+    let buffer: Vec<u16> = buffer
+        .chunks_exact(2)
+        .map(|bytes| u16::from_ne_bytes(bytes.try_into().unwrap()))
+        .collect();
+    let mut new_name = String::from_utf16(&buffer[..])?;
+    let new_root_dir_handle = v_space.read(file_info + 8, 8).await?;
+    let new_root_dir_handle = u64::from_ne_bytes(new_root_dir_handle[..].try_into().unwrap());
+    if new_root_dir_handle > 0 {
+        let root_dir = get_root_dir(
+            &v_space,
+            process,
+            new_root_dir_handle,
+            profile,
+            body_rva,
+            name_rva,
+        )
+        .await?;
+        new_name = format!(r"{}\{}", root_dir, new_name);
+    } else if new_name.starts_with(r"\??\") {
+        // full canonical path
+        // remove prefixes like \??\C:
+        let (_, rest) = new_name.split_at(r"\??\C:".len());
+        new_name = String::from(rest);
+    } else {
+        let backslash = '\\';
+        let mut fname2 = fname.clone();
+        let slash_pos = fname2.rfind(backslash).ok_or(Error::InvalidVAddr)?;
+        fname2.truncate(slash_pos + backslash.len_utf8());
+        fname2.push_str(&new_name);
+        new_name = fname2;
+    }
+
+    dom.resume_from_bp(orig, event, extra, enable_ss).await?;
+
+    // a rename event is sent as
+    // a read from the old file
+    // and a write to the new file
+    let mut message = capnp::message::Builder::new_default();
+    {
+        let mut event_log = message.init_root::<event::Builder>();
+        super::set_msg_proc(event_log.reborrow(), pid, ppid, &proc_name);
+
+        let detail = event_log.get_detail();
+        let mut file = detail.init_file();
+        file.set_name(&fname);
+        file.set_access(FileAccess::Read);
+    }
+
+    log_tx.send(message).await;
+
+    let mut message = capnp::message::Builder::new_default();
+    {
+        let mut event_log = message.init_root::<event::Builder>();
+        super::set_msg_proc(event_log.reborrow(), pid, ppid, &proc_name);
+
+        let detail = event_log.get_detail();
+        let mut file = detail.init_file();
+        file.set_name(&new_name);
+        file.set_access(FileAccess::Write);
+    }
+
+    log_tx.send(message).await;
+
+    Ok(())
+}
+
+async fn get_root_dir(
+    v_space: &IA32eVirtual,
+    process: IA32eAddrT,
+    handle: u64,
+    profile: &RekallProfile,
+    body_rva: u64,
+    name_rva: u64,
+) -> Result<String, Error> {
+    let root_dir_ptr = handle_table::get_obj_by(v_space, handle, process, profile).await?;
+    let dir_name = memory::read_utf16(&v_space, root_dir_ptr + body_rva + name_rva).await?;
+    Ok(dir_name)
 }
